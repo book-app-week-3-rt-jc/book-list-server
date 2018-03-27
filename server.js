6b6fab72f9253d7ad6ea008f3cc2c2d4fc0b019d
Richard Tae <taerichard@yahoo.com>
2018-03-27 14:39:44 -0700

    just need css

--- server.js
+++ server.js
@@ -14,13 +14,20 @@ const CLIENT_URL = process.env.CLIENT_URL;
 
 //Database Setup
 const client = new pg.Client(process.env.DATABASE_URL);
+// const client = new pg.Client('postgres://postgres:1234@localhost:5432/books_app');
 client.connect();
 client.on('error', err => console.error(err));
 
 //API Endpoints
 
-app.get('/api/v1/books', (req,res) => res.send('It lives!'));
+app.use(cors());
 
+app.get('/api/v1/books', (req,res) => {
+  client.query(
+    `SELECT book_id, title, author, image_url, isbn FROM books;`)
+    .then(results => res.send(results.rows))
+    .catch(console.error);
+});
 //This app.get will need a lot more fleshing out once the database is operational
 
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
