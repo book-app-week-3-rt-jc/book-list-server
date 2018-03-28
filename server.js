67ff760efcaa5f7c4978c0fc31f7294865547e2c
Richard Tae <taerichard@yahoo.com>
2018-03-28 13:18:21 -0700

    updated route.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//Application Dependicies 
+//Application Dependicies
 
 const express = require('express');
 const pg = require('pg');
@@ -24,15 +24,41 @@ app.use(cors());
 
 app.get('/api/v1/books', (req,res) => {
   client.query(
-    `SELECT book_id, title, author, image_url, isbn FROM books;`)
+    'SELECT book_id, title, author, image_url, isbn FROM books;')
     .then(results => res.send(results.rows))
     .catch(console.error);
 });
+
+app.get('/api/v1/books/:id', (req, res) => {
+  client.query(
+    'SELECT * FROM books WHERE book_id=$1;',
+    [req.params.id])
+    .then(results => res.send(results.rows))
+    .catch(console.error);
+});
+
+app.post('/api/v1/books', (req, res) => {
+  client.query(
+    'INSERT INTO books (title, author, isbn, image_url, description) VALUES ($1,$2,$3, $4, $5)', 
+    [
+      req.body.title,
+      req.body.author,
+      req.body.isbn,
+      req.body.image_url,
+      req.body.description
+    ])
+    .then(results => res.send(results.rows))
+    .catch(console.error);
+});
+
+
+
 //This app.get will need a lot more fleshing out once the database is operational
 
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
 
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
+
 
 
 
